f301645f1b82c51d4366f28055b7df49bcca9fca
manipapan2 <105167359+manipapan2@users.noreply.github.com>
2022-08-30 15:13:21 +0430

    a

--- MR.js
+++ MR.js
@@ -3,6 +3,10 @@ var passId = document.getElementById("passId");
 var loginbtn = document.getElementById("login-button");
 loginbtn.addEventListener("click" , check);
 document.getElementById("season1").addEventListener("click" , show)
+document.getElementById("season2").addEventListener("click" , show2)
+document.getElementById("season3").addEventListener("click" , show3)
+document.getElementById("season4").addEventListener("click" , show4)
+document.getElementById("back").addEventListener("click" , back)
 
 
 function check(){
@@ -25,7 +29,26 @@ function asli(){
         document.getElementById("audio").play();
         document.getElementById("datab").style.display="none";
         document.getElementById("seasons").style.display="flex";
+        document.getElementById("mute").style.display="none";
+
     }
+
+}
+
+
+function back(){
+    document.getElementById("seasons").style.display="flex";
+    document.getElementById("seasons").style.flexDirection="row";
+    document.getElementById("season1").style.display="flex";
+    document.getElementById("season2").style.display="flex";
+    document.getElementById("season3").style.display="flex";
+    document.getElementById("season4").style.display="flex";
+    document.getElementById("seasons").style.width="700px";
+    document.getElementById("s1").style.display="none";
+    document.getElementById("s2").style.display="none";
+    document.getElementById("s3").style.display="none";
+    document.getElementById("s4").style.display="none";
+    document.getElementById("divB").style.display="none";
 
 }
 
@@ -36,18 +59,46 @@ function show(){
     document.getElementById("season3").style.display="none";
     document.getElementById("season4").style.display="none";
     document.getElementById("seasons").style.width="1400px";
-    const nodeList = document.querySelectorAll(".season1");
-    for (let i = 0; i < nodeList.length; i++) {
-    nodeList[i].style.display = "flex";
-    }
+    document.getElementById("s1").style.display="flex";
+    document.getElementById("seasons").style.flexDirection="column";
+    document.getElementById("divB").style.display="flex";
+
     
 }
 
+function show2(){
+    document.getElementById("season1").style.display="none";
+    document.getElementById("season2").style.display="none";
+    document.getElementById("season3").style.display="none";
+    document.getElementById("season4").style.display="none";
+    document.getElementById("seasons").style.width="1400px";
+    document.getElementById("s2").style.display="flex";
+    document.getElementById("seasons").style.flexDirection="column";
+    document.getElementById("divB").style.display="flex";
+    
 
-function vid(){
-    const nodeList = document.querySelectorAll(".season1");
-    for (let i = 0; i < nodeList.length; i++) {
-    nodeList[i].style.display = "none";
-    }
-    document.getElementById("vid").style.display="flex";
+}
+
+
+function show3(){
+    document.getElementById("season1").style.display="none";
+    document.getElementById("season2").style.display="none";
+    document.getElementById("season3").style.display="none";
+    document.getElementById("season4").style.display="none";
+    document.getElementById("seasons").style.width="1400px";
+    document.getElementById("s3").style.display="flex";
+    document.getElementById("seasons").style.flexDirection="column";
+    document.getElementById("divB").style.display="flex";
+}
+
+
+function show4(){
+    document.getElementById("season1").style.display="none";
+    document.getElementById("season2").style.display="none";
+    document.getElementById("season3").style.display="none";
+    document.getElementById("season4").style.display="none";
+    document.getElementById("seasons").style.width="1400px";
+    document.getElementById("s4").style.display="flex";
+    document.getElementById("seasons").style.flexDirection="column";
+    document.getElementById("divB").style.display="flex";
 }
